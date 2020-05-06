1160d8d548c19722239f39761fce666a2e8c0a52
LIM KWANG KYU <kk.lim@gsshop.com>
2020-05-06 14:20:09 +0900

    feat : 비동기 작업 상태 관리 하기 - 코드 리팩토링

--- api-inegrate/src/UsersContext.js
+++ api-inegrate/src/UsersContext.js
@@ -1,36 +1,14 @@
 import React, { createContext, useReducer, useContext } from "react";
-import axios from "axios";
+import * as api from "./api";
+import createAsyncDispatcher, {
+  createAsyncHandler,
+  initialAsyncState,
+} from "./asyncActionUtils";
 
 const initalState = {
-  users: {
-    loading: false,
-    data: null,
-    error: null,
-  },
-  user: {
-    loading: false,
-    data: null,
-    error: null,
-  },
+  users: initialAsyncState,
+  user: initialAsyncState,
 };
-
-const loadingState = {
-  loading: true,
-  data: null,
-  error: null,
-};
-
-const sucess = (data) => ({
-  loading: false,
-  data,
-  error: null,
-});
-
-const error = (e) => ({
-  loading: false,
-  data: null,
-  error: e,
-});
 
 // GET_USERS
 // GET_USERS_SUCCESS
@@ -39,45 +17,28 @@ const error = (e) => ({
 // GET_USER_SUCCESS
 // GET_USER_ERROR
 
+const usersHandler = createAsyncHandler("GET_USERS", "users");
+const userHandler = createAsyncHandler("GET_USER", "user");
+
 const userReducer = (state, action) => {
   switch (action.type) {
     case "GET_USERS":
-      return {
-        ...state,
-        users: loadingState,
-      };
     case "GET_USERS_SUCCESS":
-      return {
-        ...state,
-        users: sucess(action.data),
-      };
     case "GET_USERS_ERROR":
-      return {
-        ...state,
-        users: error(action.error),
-      };
+      return usersHandler(state, action);
     case "GET_USER":
-      return {
-        ...state,
-        user: loadingState,
-      };
     case "GET_USER_SUCCESS":
-      return {
-        ...state,
-        user: sucess(action.data),
-      };
     case "GET_USER_ERROR":
-      return {
-        ...state,
-        user: error(action.error),
-      };
+      return userHandler(state, action);
+    default:
+      throw new Error("Unhandled action type", action.type);
   }
 };
 
 const UsersStateContext = createContext(null);
 const UsersDispatchContext = createContext(null);
 
-const UsersProvider = ({ children }) => {
+export const UsersProvider = ({ children }) => {
   const [state, dispatch] = useReducer(userReducer, initalState);
   return (
     <UsersStateContext.Provider value={state}>
@@ -96,30 +57,32 @@ const useMyContext = (ctx) => {
   return context;
 };
 
-const useUsersState = () => useMyContext(UsersStateContext);
-const useUsersDispatch = () => useMyContext(UsersDispatchContext);
+export const useUsersState = () => useMyContext(UsersStateContext);
+export const useUsersDispatch = () => useMyContext(UsersDispatchContext);
 
-const getUsers = async (dispatch) => {
-  dispatch({ type: "GET_USERS" });
-  try {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    dispatch({ type: "GET_USERS_SUCCESS", data: response.data });
-  } catch (e) {
-    dispatch({ type: "GET_USERS_ERROR", error: e });
-  }
-};
-const getUser = async (dispatch, id) => {
-  dispatch({ type: "GET_USER" });
-  try {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/users/${id}`
-    );
-    dispatch({ type: "GET_USER_SUCCESS", data: response.data });
-  } catch (e) {
-    dispatch({ type: "GET_USER_ERROR", error: e });
-  }
-};
+// const getUsers = async (dispatch) => {
+//   dispatch({ type: "GET_USERS" });
+//   try {
+//     const response = await axios.get(
+//       "https://jsonplaceholder.typicode.com/users"
+//     );
+//     dispatch({ type: "GET_USERS_SUCCESS", data: response.data });
+//   } catch (e) {
+//     dispatch({ type: "GET_USERS_ERROR", error: e });
+//   }
+// };
+// const getUser = async (dispatch, id) => {
+//   dispatch({ type: "GET_USER" });
+//   try {
+//     const response = await axios.get(
+//       `https://jsonplaceholder.typicode.com/users/${id}`
+//     );
+//     dispatch({ type: "GET_USER_SUCCESS", data: response.data });
+//   } catch (e) {
+//     dispatch({ type: "GET_USER_ERROR", error: e });
+//   }
+// };
 
-export { UsersProvider, useUsersState, useUsersDispatch, getUsers, getUser };
+// export { UsersProvider, useUsersState, useUsersDispatch, getUsers, getUser };
+export const getUsers = createAsyncDispatcher("GET_USERS", api.getUsers);
+export const getUser = createAsyncDispatcher("GET_USER", api.getUser);
