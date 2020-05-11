89e751c165f3b4ccdc48a06a99fa0eb466e5d23d
LIM KWANG KYU <kk.lim@gsshop.com>
2020-05-11 17:10:43 +0900

    34. 6장 | 벨로퍼트와 함께 하는 모던 리액트 : Redux - 김민준 강사

---
+++ learn-redux/src/exercise.js
@@ -0,0 +1,64 @@
+import { createStore } from "redux";
+
+const initalState = {
+  counter: 0,
+  text: "",
+  list: [],
+};
+
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+const CHANGE_TEXT = "CHANGE_TEXT";
+const ADD_TO_LIST = "ADD_TO_LIST";
+
+const increase = () => ({ type: INCREASE });
+const desrease = () => ({ type: DECREASE });
+const changeText = (text) => ({ type: CHANGE_TEXT, text });
+const addToList = (item) => ({ type: ADD_TO_LIST, item });
+
+const reducer = (state = initalState, action) => {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    case CHANGE_TEXT:
+      return {
+        ...state,
+        text: action.text,
+      };
+    case ADD_TO_LIST:
+      return {
+        ...state,
+        list: state.list.concat(action.item),
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+const listener = () => {
+  const state = store.getState();
+  console.log(state);
+};
+
+const unsubscribe = store.subscribe(listener);
+// unsubscribe()
+
+store.dispatch(increase());
+store.dispatch(desrease());
+store.dispatch(changeText("안녕하세요"));
+store.dispatch(addToList({ id: 1, text: "와우" }));
+
+window.store = store;
+window.unsubscribe = unsubscribe;
